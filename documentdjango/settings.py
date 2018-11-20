3d73fcf33b94b1b3054d4d4ede32e8b8dc9046b0
tongyongquan <1070969926@qq.com>
2018-11-20 16:59:05 +0800

    allow all

--- documentdjango/settings.py
+++ documentdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '68t3^4#fzo3j7t%l&tiuaxic&ihzp0sgqr&@*!i(ve!!+yzc58'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
