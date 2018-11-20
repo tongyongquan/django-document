515d169e5ab852a878639545dc86b5f0ca5d0beb
tongyongquan <1070969926@qq.com>
2018-11-20 19:01:10 +0800

    static files

--- documentdjango/settings.py
+++ documentdjango/settings.py
@@ -123,15 +123,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = 'www/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT ='/www/static'
 
 # 配置静态文件目录
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-    ]
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static').replace('\\', '/'),
+)
 
-MEDIA_URL = 'www/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/www/media'
 
 # 分页配置
 PAGE_NUM = 5
