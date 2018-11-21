ee978f8e8bb32d7cbab5ed1647f004805f8e0943
tongyongquan <1070969926@qq.com>
2018-11-21 18:27:39 +0800

    前端

--- documentdjango/settings.py
+++ documentdjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '68t3^4#fzo3j7t%l&tiuaxic&ihzp0sgqr&@*!i(ve!!+yzc58'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
