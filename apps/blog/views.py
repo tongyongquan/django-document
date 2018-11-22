0c916f9dd82d19422474573886eec0d59752d99d
tongyongquan <1070969926@qq.com>
2018-11-22 18:27:44 +0800

    home page

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -6,7 +6,11 @@ from django.conf import settings
 
 categories = Category.objects.all()  # 获取全部的分类对象
 tags = Tag.objects.all()  # 获取全部的标签对象
-months = Article.objects.datetimes('pub_time', 'month', order='DESC')
+pub_dates = Article.objects.datetimes('pub_time', 'month', order='DESC')
+months = {}
+for pub_date in pub_dates:
+    count = Article.objects.filter(pub_time__year=pub_date.year, pub_time__month=pub_date.month).count()
+    months.update({pub_date: count})
 
 
 def home(request):  # 主页
@@ -19,13 +23,14 @@ def home(request):  # 主页
         post_list = paginator.page(1)
     except EmptyPage:
         post_list = paginator.page(paginator.num_pages)
-    return render(request, 'home.html', {'post_list': post_list,'category_list': categories, 'months': months})
+    return render(request, 'home.html',
+                  {'post_list': post_list, 'category_list': categories, 'months': months, 'tag_list': tags})
 
 
 def detail(request, id):  # 查看文章详情
     try:
         post = Article.objects.get(id=str(id))
-        post.viewed()   # 更新浏览次数
+        post.viewed()  # 更新浏览次数
         tags = post.tags.all()  # 获取文章对应所有标签
         next_post = post.next_article()  # 上一篇文章对象
         prev_post = post.prev_article()  # 下一篇文章对象
@@ -82,6 +87,6 @@ def archives(request, year, month):
         'post_list': post_list,
         'category_list': categories,
         'months': months,
-        'year_month': year+'年'+month+'月'
-        }
-    )
+        'year_month': year + '年' + month + '月'
+    }
+                  )
