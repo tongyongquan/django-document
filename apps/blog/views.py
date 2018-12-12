880794d1bb464a10bd6091ca31c9f121b81994be
TONG <32868097+tongyongquan@users.noreply.github.com>
2018-12-12 14:18:11 +0800

    热门阅读

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -11,10 +11,11 @@ try:
     categories = Category.objects.all()  # 获取全部的分类对象
     tags = Tag.objects.all()  # 获取全部的标签对象
     pub_dates = Article.objects.datetimes('pub_time', 'month', order='DESC')
-    months = {}
-    for pub_date in pub_dates:
-        count = Article.objects.filter(pub_time__year=pub_date.year, pub_time__month=pub_date.month).count()
-        months.update({pub_date: count})
+    hot_articles = Article.objects.filter().order_by('-views')[:10]
+    # months = {}
+    # for pub_date in pub_dates:
+    #     count = Article.objects.filter(pub_time__year=pub_date.year, pub_time__month=pub_date.month).count()
+    #     months.update({pub_date: count})
 except Exception as e:
     print(e)
 
@@ -30,14 +31,14 @@ def home(request):  # 主页
     except EmptyPage:
         post_list = paginator.page(paginator.num_pages)
     return render(request, 'home.html',
-                  {'post_list': post_list, 'category_list': categories, 'months': months, 'tag_list': tags})
+                  {'post_list': post_list, 'category_list': categories, 'hot_articles': hot_articles, 'tag_list': tags})
 
 
 def detail(request, id):  # 查看文章详情
     try:
         post = Article.objects.get(id=str(id))
         post.viewed()  # 更新浏览次数
-        tags = post.tags.all()  # 获取文章对应所有标签
+        article_tags = post.tags.all()  # 获取文章对应所有标签
         next_post = post.next_article()  # 上一篇文章对象
         prev_post = post.prev_article()  # 下一篇文章对象
     except Article.DoesNotExist:
@@ -45,8 +46,8 @@ def detail(request, id):  # 查看文章详情
     return render(request, 'post.html',
                   {
                       'post': post,
-                      'tags': tags,
-                      'category_list': categories,
+                      'category_list': categories, 'hot_articles': hot_articles, 'tag_list': tags,
+                      'tags': article_tags,
                       'next_post': next_post,
                       'prev_post': prev_post
                   })
@@ -63,7 +64,9 @@ def search_category(request, id):  # 分类搜索
         post_list = paginator.page(1)
     except EmptyPage:
         post_list = paginator.page(paginator.num_pages)
-    return render(request, 'category.html', {'post_list': post_list, 'category_list': categories, 'category': category})
+    return render(request, 'category.html',
+                  {'post_list': post_list, 'category_list': categories, 'hot_articles': hot_articles, 'tag_list': tags,
+                   'category': category})
 
 
 def search_tag(request, tag):  # 标签搜索
@@ -76,7 +79,9 @@ def search_tag(request, tag):  # 标签搜索
         post_list = paginator.page(1)
     except EmptyPage:
         post_list = paginator.page(paginator.num_pages)
-    return render(request, 'tag.html', {'post_list': post_list, 'category_list': categories, 'tag': tag})
+    return render(request, 'tag.html',
+                  {'post_list': post_list, 'category_list': categories, 'hot_articles': hot_articles, 'tag_list': tags,
+                   'tag': tag})
 
 
 def archives(request, year, month):
@@ -91,8 +96,7 @@ def archives(request, year, month):
         post_list = paginator.page(paginator.num_pages)
     return render(request, 'archive.html', {
         'post_list': post_list,
-        'category_list': categories,
-        'months': months,
+        'category_list': categories, 'hot_articles': hot_articles, 'tag_list': tags,
         'year_month': year + '年' + month + '月'
     }
                   )
@@ -140,6 +144,7 @@ def manager_register(request):
             return render(request, 'register.html', args)
 
     return render(request, 'register.html')
+
 
 @login_required()
 def manager_logout(request):
